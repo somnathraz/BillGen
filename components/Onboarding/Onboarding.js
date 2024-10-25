247d67a61cf4f6501137bddb39f55b717d53d995
somanath khadanga <31034311+somnathraz@users.noreply.github.com>
2024-10-25 12:48:20 +0530

    added navigation and screens

---
+++ components/Onboarding/Onboarding.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Onboarding = () => {
+  return (
+    <View>
+      <Text>onboarding</Text>
+    </View>
+  );
+};
+
+export default Onboarding;
