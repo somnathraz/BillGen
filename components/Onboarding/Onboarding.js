57acbd8e6e820701b4a01eeb8032a7d74b2c2eef
gshailyy <shaily@outspokn.co>
2024-11-05 12:12:35 +0530

    onboarding screen

--- components/Onboarding/Onboarding.js
+++ components/Onboarding/Onboarding.js
@@ -6,6 +6,9 @@ import {
   Platform,
   SafeAreaView,
   Dimensions,
+  Image,
+  Button,
+  TouchableOpacity,
 } from 'react-native';
 import Theme from '../../Theme/Theme';
 const {width, height} = Dimensions.get('window');
@@ -13,12 +16,32 @@ const Onboarding = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
-        <Text style={styles.heading}>
-          Manage Business Easily and Be More Organized
-        </Text>
-        <Text style={styles.desc}>
-          Business Management has been easier, more Organized and Tidy
-        </Text>
+        <View style={styles.bigBox}>
+          <Text style={styles.heading}>
+            New approach for your Finance
+          </Text>
+          <Text style={styles.desc}>
+          Now your finances are in one place and always under control
+          </Text>
+          <View style={styles.imgWrapper}>
+            <Image source={require('../../assets/fonts/Images/Onboarding1.png')}
+            style={styles.OnboardingImg}/>
+          </View>
+        </View>
+        <View style={styles.smallBox}>
+          <TouchableOpacity style={styles.button} // Apply custom styles here
+            onPress={() => {
+              // Handle button press
+            }}
+          >
+            <Text style={styles.buttonText}>Get Started</Text>
+          </TouchableOpacity>
+          <View style={styles.dotsContainer}>
+            <View style={styles.dotActive} />
+            <View style={styles.dot} />
+            <View style={styles.dot} />
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -33,20 +56,93 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingHorizontal: (width * 3.5) / 100,
+    paddingTop:(width*8/100),
+    
+    gap:20,
+  },
+  bigBox:{
+    backgroundColor: '#CDE3F3',
+    paddingHorizontal: (width*3.5/100),
+    paddingTop: height*5/100,
+    borderRadius: height*3/100,
+    height: height*60/100,
+  },
+  smallBox:{
+    backgroundColor: '#CDE3F3',
+    paddingHorizontal: (width*6/100),
+    paddingVertical: height*3/100,
+    borderRadius: height*3/100,
+    
+    // height: height*10/100,
+    flexDirection:'column',
+    justifyContent:'center',
     alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: (width * 5) / 100,
   },
   heading: {
-    fontFamily: Theme.fonts.extraBold.fontFamily,
-    fontSize: 30,
+    fontFamily: Theme.fonts.semiBold.fontFamily,
+    fontSize: 56,
+    lineHeight:60,
     color: '#000',
-    textAlign: 'center',
-    fontWeight: Platform.OS === 'ios' ? '700' : 'normal',
+    textAlign: 'left',
+    // fontWeight: Platform.OS === 'ios' ? '700' : 'normal',
   },
   desc: {
     fontFamily: Theme.fonts.regular.fontFamily,
     color: Theme.colors.descText,
-    textAlign: 'center',
+    textAlign: 'left',
+    fontSize: 20,
+    lineHeight:28,
+    // paddingLeft:width*2.5/100,
+  },
+  imgWrapper:{
+    // position:'absolute',
+    // right:0,
+    // bottom: 0,
+    marginTop: height*31/100,
+    marginRight: -(width*3.5/100),
+  },
+  OnboardingImg:{
+    alignSelf:'flex-end',
+    position:'absolute',
+    right:0,
+    bottom: 0,
+  },
+  button:{
+    backgroundColor: 'black',
+    flexDirection:"row",
+    justifyContent:'center',
+    alignItems: 'center',
+    borderRadius: height*2/100,
+    paddingHorizontal: (width*3.5/100),
+    paddingVertical: 15,
+    width: '100%',
+  },
+  buttonText:{
+    fontFamily: Theme.fonts.regular.fontFamily,
+    color: 'white',
+    fontSize: 20,
+  },
+  dotsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems:'center',
+    marginTop: 20, // Space above the dots
+  },
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 5,
+    backgroundColor: Theme.colors.primary, // Inactive dot color
+    marginHorizontal: 5,
+  },
+  dotActive: {
+    width: 11,
+    height: 11,
+    borderRadius: 5,
+    backgroundColor: Theme.colors.primary, // Active dot color
+    marginHorizontal: 5,
   },
 });
